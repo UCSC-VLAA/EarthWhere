f3464afb029a1e50fff29e2ae4fc51c068e6ff9a
Tony Qian <56781672+Batwho@users.noreply.github.com>
2025-10-10 01:25:13 -0400

    Update eval_all.sh

--- eval_all.sh
+++ eval_all.sh
@@ -2,8 +2,9 @@ echo "Inference Gemini-2.5-flash on WhereCountry ..."
 
 export GEMINI_API_KEY="YOUR_GEMINI_API_KEY_HERE"
 
+cd wherecountry
 python ./wherecountry/eval_gemini_25_flash.py --model gemini-2.5-flash --csv_path "./wherecountry/wherecountry_500.csv" --img_dir "./wherecountry/imgs" --output_csv "./results/overall/gemini_25_flash.csv" --api_key $GEMINI_API_KEY
-
+cd ../wherestreet
 echo "Inference Gemini-2.5-flash on WhereStreet ..."
 
 # export GEMINI_API_KEY="YOUR_GEMINI_API_HERE"
